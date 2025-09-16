6be9442af81a0659c64d51f3e1c4870a19244bdf
David Nelson <thedoorman27@yahoo.com>
2025-09-16 13:09:17 -0600

    added py file

---
+++ Hello World/HelloWorld.py
@@ -0,0 +1,18 @@
+from colorama import Fore
+import time
+
+x=0
+while x<20:
+    print(Fore.RED + "Hello World!")
+    time.sleep(0.1)
+    print(Fore.BLUE + "Hello World!")
+    time.sleep(0.1)
+    print(Fore.YELLOW + "Hello World!")
+    time.sleep(0.1)
+    print(Fore.GREEN + "Hello World!")
+    time.sleep(0.1)
+    print(Fore.BLUE + "Hello World!")
+    time.sleep(0.1)
+    print(Fore.MAGENTA + "Hello World!")
+    time.sleep(0.1)
+    x +=1
